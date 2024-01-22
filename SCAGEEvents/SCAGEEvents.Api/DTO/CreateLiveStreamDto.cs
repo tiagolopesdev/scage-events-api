07c6975f3fe5b15dc2c8543a56f54b2a891a31a2
Tiago Lopes <saxtiago14@gmail.com>
2024-01-22 15:35:36 -0300

    Refactor: unnecessary line removed

--- SCAGEEvents/SCAGEEvents.Api/DTO/CreateLiveStreamDto.cs
+++ SCAGEEvents/SCAGEEvents.Api/DTO/CreateLiveStreamDto.cs
@@ -14,34 +14,3 @@ namespace SCAGEEvents.Api.DTO
         //public FileStream Thumbnails { get; set; }  
     }
 }
-/*
-{
-    "kind": "youtube#liveBroadcast",
-    "snippet": {
-        "scheduledStartTime": "2024-01-22T19:00:00",
-        "channelId": "UCYuiMRv-pAEcPcqPAuUl_wg",
-        "title": "Test create live stream",
-        "description": "Description test live stream",
-        "thumbnails": {
-            "default": {
-                "url": "https://drive.google.com/file/d/1VCK74_DkpkqwKI9yptvJhCScEBxh_wf2/view?usp=drive_link",
-                "width": 120,
-                "height": 90
-            },
-            "medium": {
-                "url": "https://drive.google.com/file/d/1VCK74_DkpkqwKI9yptvJhCScEBxh_wf2/view?usp=drive_link",
-                "width": 320,
-                "height": 180
-            },
-            "high": {
-                "url": "https://drive.google.com/file/d/1VCK74_DkpkqwKI9yptvJhCScEBxh_wf2/view?usp=drive_link",
-                "width": 480,
-                "height": 360
-            }
-        }
-    },
-    "status": {
-        "privacyStatus": "unlisted"
-    }
-}
- */
